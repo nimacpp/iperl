288eb56db867fd3ad9bb2ca1dfd4ba4c4c4451ce
Nima <87241816+nimacpp@users.noreply.github.com>
2024-09-25 23:32:51 +0330

    updating ...

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# iperl
-An enhanced Interactive perl. 
+An enhanced Interactive Perl shell. 
+# install in linux
+
+```bash
+git clone https://github.com/nimacpp/iperl.git
+cd iperl
+./install.sh
+```
+
+# View
+![[iperl.png]]
+# Usage
+in terminal
+```
+iperl
+```
--- install.sh
+++ install.sh
@@ -12,10 +12,8 @@ then
     echo -e "${RED}[-] perl could not be found .${RST}"
     echo "Please install perl ."
 	exit
-else
-		echo -e "${GREEN}[+] perl could be found .${RST}"
 fi 
-echo -e "${GREEN} Start install ... (${YELLOW} pass your password ${RST})"
-sudo cp iperl /bin
-echo -e "${GREEN} [+] iperl installed .${RST}"
+echo -e "${GREEN} Start install ...${RST}"
+cp iperl /usr/local/bin/iperl
+echo -e "${GREEN}[+] iperl installed .${RST}"
  
